be05690eb239b678b5e0756cf406d0077c7b38e8
cristiano <cristiano.souza.mendonca@gmail.com>
2015-06-04 21:33:47 -0300

    Corrigindo controller de retorno do FB

--- app/controllers/customer_omniauth_callbacks_controller.rb
+++ app/controllers/customer_omniauth_callbacks_controller.rb
@@ -1,7 +1,27 @@
 class CustomerOmniauthCallbacksController < Devise::OmniauthCallbacksController
    def facebook
     current_customer.save_provider_uid(request.env["omniauth.auth"])
-    current_customer.photo_logs.not_posted.map { |photo| photo.submit_to_fb }
-    redirect_to new_photo_log_path
+    postedPhotos = post(pendingPostings).try(:compact)
+    rwd = current_customer.get_rewards_by(postedPhotos) unless postedPhotos.blank?
+
+    msg = "PARABÉNS! Suas fotos foram enviadas com sucesso. "
+    msg = msg + "Recompensa ganha: #{rwd}" unless rwd.zero?
+
+    flash[:success] = msg
+    redirect_to customer_root_path, flash: { success: msg }
+  end
+
+  private
+
+  def post(pendingPostings)
+    prof = pendingPostings.first.schedule.professional
+    pendingPostings.each do |photo|
+      begin
+        photo.submit_to_fb(prof)
+      rescue Koala::Facebook::APIError => e
+        logger.info e.to_s
+        nil
+      end
+    end
   end
 end

6df4a53122440368f39ca8a5267cb4d0f4e34c40
cristiano <cristiano.souza.mendonca@gmail.com>
2015-10-19 21:28:13 -0200

    utilizando master para deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :domain, '45.55.237.28'
 set :deploy_to, '/home/safira/'
 set :repository, 'https://cristianocasm@bitbucket.org/cristianocasm/safirasaloes.git'
 # set :branch, 'master'
-set :branch, 'diversos'
+set :branch, 'master'
 set :rollbar_access_token, "ec9ca75181824170925fa07796df7e56"
 set :rails_env, 'production'
 set :sidekiq_log, "#{deploy_to}#{shared_path}/log/sidekiq.log"
